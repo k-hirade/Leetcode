1539d06d5ada6f2c9b119afa3bda6202998ec1d1
k-hirade <keishi.hirade@osaro.com>
2023-10-02 17:39:00 +0900

    21

---
+++ Easy/21 - Merge Two Sorted Lists.py
@@ -0,0 +1,24 @@
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        dummy = ListNode(-1)
+        prev = dummy
+
+        while list1 and list2:
+            if list1.val <= list2.val:
+                prev.next = list1
+                list1 = list1.next
+            else:
+                prev.next = list2
+                list2 = list2.next
+            prev = prev.next
+
+        # どちらかのリストがまだ要素を持っていれば、それを結果に追加
+        prev.next = list1 if list1 else list2
+
+        return dummy.next
