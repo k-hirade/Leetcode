fb421a8f29e6e673aea4f1ff412d0cc90b7b7814
k-hirade <keishi.hirade@osaro.com>
2023-10-11 11:11:17 +0900

    26, 27

--- Easy/26 - Remove Duplicates from Sorted Array.py
+++ Easy/26 - Remove Duplicates from Sorted Array.py
@@ -1,2 +1,11 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        i, j = 1, 1
+        while i < len(nums):
+            if nums[i] != nums[i-1]:
+                nums[j] = nums[i]
+                j += 1
+            i += 1
+        return j
