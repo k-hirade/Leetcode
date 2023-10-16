31c93f044c3677385a8fb5fa79423a5dcb12c061
k-hirade <keishi.hirade@osaro.com>
2023-10-16 15:53:25 +0900

    change name and 121

--- Easy/118 - Pascal's Triangle.py
+++ Easy/118 - Pascal's Triangle.py
@@ -1,2 +1,13 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
+        answer_arry = []
+        for row_num in range(numRows):
+            row = [None for _ in range(row_num+1)]
+            row[0], row[-1] = 1, 1
+            for j in range(1, len(row)-1):
+                row[j] = answer_arry[row_num-1][j-1] + \
+                    answer_arry[row_num-1][j]
+
+            answer_arry.append(row)
+
+        return answer_arry
